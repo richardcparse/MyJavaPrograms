aed0f2cee79766d59a237c73c0f7db7e64ab358c
Richard <richardcparse@users.noreply.github.com>
2017-07-31 11:16:06 +0200

    Add files via upload

---
+++ BlodTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BlodTest
+
+	{
+
+
+	public static void main(String[] args)
+
+		{
+
+	
+		junit.textu1.TestRunner.run( new JUnit4TestAdapter(BlodTest.class));
+
+
+	}
+
+
+}
